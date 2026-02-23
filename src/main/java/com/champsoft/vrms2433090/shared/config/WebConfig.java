bd72e4c6dff5c590535b23b87fa83a43bb2ae8a4
Maxime Bergeron <pmmaster2020@gmail.com>
2026-02-22 22:19:25 -0500

    Class-Activity 3 Second push

--- src/main/java/com/champsoft/vrms2433090/shared/config/WebConfig.java
+++ src/main/java/com/champsoft/vrms2433090/shared/config/WebConfig.java
@@ -1,5 +1,27 @@
 package com.champsoft.vrms2433090.shared.config;
 
-public class WebConfig {
-    // TODO: CORS, interceptors, formatters (optional)
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new WebContentInterceptor());
+        registry.addInterceptor(new UserRoleAuthorizationInterceptor());
+        registry.addInterceptor(new LocaleChangeInterceptor());
+    }
 }
